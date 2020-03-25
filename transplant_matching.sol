35c600e2645643c4ad2a3991af0853c7c6c9f50c
leokfwong <leo.wong2@mail.mcgill.ca>
2020-03-25 19:52:09 -0400

    Update solidity

--- transplant_matching.sol
+++ transplant_matching.sol
@@ -1,7 +1,7 @@
 //pragma experimental ABIEncoderV2
 pragma solidity ^0.6.1;
 
-contract TransplanMatching {
+contract TransplantMatching {
     
     uint public patientCount = 0;
     mapping(uint => Patient) public patients;
@@ -14,13 +14,17 @@ contract TransplanMatching {
         address patient_address;
     }
     
-    function addPatient(string memory _firstname, string memory _lastname, string memory _bloodtype) public {
-        patientCount++;
-        patients[patientCount] = Patient(patientCount, _firstname, _lastname, _bloodtype, msg.sender);
+    function addPatient(string memory _firstname, string memory _lastname, string memory _bloodtype) public returns (string memory){
+        if (bytes(_firstname).length == 0) {
+            return "Required fields cannot be empty.";
+        } else {
+            patientCount++;
+            patients[patientCount] = Patient(patientCount, _firstname, _lastname, _bloodtype, msg.sender);
+            return "success";
+        }
     }
     
-    
-    function getAllPatients(uint  _index) public view returns (string memory, string memory, string memory) {
+    function getAllPatients(uint _index) public view returns (string memory, string memory, string memory) {
         uint index = _index;
         return (patients[index].firstname, patients[index].lastname, patients[index].bloodtype);
     }
